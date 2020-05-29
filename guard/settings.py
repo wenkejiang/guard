2deb8d1eb83a9a780114c6cd11ccefab3e24627b
jiangwenke <jiangwenke@qding.me>
2020-05-29 14:04:32 +0800

    修改setting.xml文件，换成mysql数据库 新增忽略文件

--- guard/settings.py
+++ guard/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'guard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guard',
+        'USER': 'root',
+        'PASSWORD': 'Dkaixy@917810',
+        'HOST': '106.13.238.8',
+        'PORT': '3306'
     }
 }
 
